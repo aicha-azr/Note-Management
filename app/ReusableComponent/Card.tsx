8938bfcf5c160cdea4cbb3d4cffd5b17ad10c6d3
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-02-28 11:46:13 +0100

    card notes

--- app/ReusableComponent/Card.tsx
+++ app/ReusableComponent/Card.tsx
@@ -4,16 +4,25 @@ import { ReactNode } from 'react';
 
 interface CardProps {
   className?: string;
-  children: ReactNode;
+  title:string;
+  body: string;
+  createdAt:string;
 }
 
-const Card: React.FC<CardProps> = ({ className, children, ...rest }) =>{
+const Card: React.FC<CardProps> = ({ className,body,createdAt,title, ...rest }) =>{
     return(
-        <>
-        <div className={cn(className, "shadow-md  rounded-sm p-2 bg-[#232323] text-white")} {...rest}>
-        {children}
+        // <>
+        // <div className={cn(className, "shadow-md  rounded-sm p-2 bg-[#232323] text-white")} {...rest}>
+        // {children}
+        // </div>
+        // </>
+        <div className="max-w-md mx-auto bg-pastell-blue rounded-md overflow-hidden shadow-lg p-3">
+          <div className="p-2">
+            <h2 className="text-xl text-blanc-casse font-semibold mb-2">{title}</h2>
+            <p className="text-blanc-casse mb-4">{body}</p>
+            <p className="text-sm text-blanc-casse">{createdAt}</p>
+          </div>
         </div>
-        </>
     )
 }
 export default Card;
