454cc9cbde03dbdd43adf3429b0192fa7f63b8d7
salimaelayy <64556840+salimaelayy@users.noreply.github.com>
2024-02-28 15:19:05 +0100

    NMP-20 schema

---
+++ app/Connection/connection.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from'dotenv';
+dotenv.config();
+
+
+ export default function Connect(){ 
+mongoose.connect(process.env.URL!)
+.then(()=>console.log('you are connected'))
+.catch((e)=>console.log(e.message))
+}
