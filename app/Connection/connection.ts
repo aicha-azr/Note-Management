0656162a10e6b0cba5738fe34c0d184c5a2d70ef
aicha-azr <aichaazeroual03@gmail.com>
2024-02-28 12:04:49 +0100

    connect to database

--- app/Connection/connection.ts
+++ app/Connection/connection.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from'dotenv';
+dotenv.config();
  export default function Connect(){ 
-mongoose.connect('mongodb+srv://aichaazeroual03:NnweUCLisjodRfX2@cluster0.jyonyz5.mongodb.net/')
+mongoose.connect(process.env.URL!)
 .then(()=>console.log('you are connected'))
 .catch((e)=>console.log(e.message))
 }
